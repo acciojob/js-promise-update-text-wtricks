1bc06d43e10b6840242a795c7337ecf81db2d7ef
Anuj Kumar <101552403+wtricks@users.noreply.github.com>
2023-08-25 09:02:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// Function that returns a promise which resolves after 1 second.
+function delayedHello() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+// Using the promise to update the text of the HTML element.
+delayedHello().then((result) => {
+  const outputDiv = document.getElementById("output");
+  outputDiv.textContent = result;
+});
